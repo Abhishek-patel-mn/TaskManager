8e542cc2bcd37b24b9f6e6cfd252b2c8226b3018
Abhishek-patel-mn <abhipatel6493@gmail.com>
2018-05-30 22:06:37 +0530

    Angular 6

--- ATM/angular/src/app/app.module.ts
+++ ATM/angular/src/app/app.module.ts
@@ -3,22 +3,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 const appRoutes = [
-	{ path: 'login', component: LoginComponent },
-	{ path: 'register', component: RegisterComponent },
-	{ path: 'dashboard', component: DashboardComponent }
+	
 ]
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
